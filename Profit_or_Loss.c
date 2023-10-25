612f29cf3fabed9e9c9312bac7f98af259e52171
varun1716 <148329234+varun1716@users.noreply.github.com>
2023-10-25 20:22:39 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(x<y)     {         printf("Profit");     }     else if(x>y)     {         printf("Loss");     }     else if(x=y)     {         printf("No Profit and No Loss");     } }

---
+++ Profit_or_Loss.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(x<y)
+    {
+        printf("Profit");
+    }
+    else if(x>y)
+    {
+        printf("Loss");
+    }
+    else if(x=y)
+    {
+        printf("No Profit and No Loss");
+    }
+}
