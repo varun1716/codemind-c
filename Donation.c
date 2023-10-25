fb049ad7eaab2ded39063e3adc219d217353e3c8
varun1716 <148329234+varun1716@users.noreply.github.com>
2023-10-25 07:19:08 +0530

    #include<stdio.h> int main() {     int x,y,d;     scanf("%d%d",&x,&y);     if(y>x,d=y-x)     {         printf("%d",d);     }     else     {         printf("%d",d);     } }

---
+++ Donation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,d;
+    scanf("%d%d",&x,&y);
+    if(y>x,d=y-x)
+    {
+        printf("%d",d);
+    }
+    else
+    {
+        printf("%d",d);
+    }
+}
