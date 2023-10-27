1f1a47d95599dbe3ee23fd6727ccee05fe2a61c2
varun1716 <148329234+varun1716@users.noreply.github.com>
2023-10-27 18:38:15 +0530

    #include<stdio.h> int main() {     int x1,y1,x2,y2,a,b;     scanf("%d%d%d%d",&x1,&y1,&x2,&y2);     a=x1+y1;     b=x2+y2;     if(a<b)     {         printf("%d",a);     }     else     {         printf("%d",b);     } }

---
+++ Interior_Design.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x1,y1,x2,y2,a,b;
+    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+    a=x1+y1;
+    b=x2+y2;
+    if(a<b)
+    {
+        printf("%d",a);
+    }
+    else
+    {
+        printf("%d",b);
+    }
+}
