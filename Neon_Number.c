b3411cd1bdb163447cd13f1c9b3ca66613680320
varun1716 <148329234+varun1716@users.noreply.github.com>
2023-11-06 16:06:20 +0530

    #include<stdio.h> int main() { 	int n,r,sum=0,sq; 	scanf("%d",&n); 	sq=n*n; 	while(sq!=0) 	{ 		r=sq%10; 		sum=sum+r; 		sq=sq/10; 	}
    	if(sum==n)
    	{
    		printf("Neon Number");
    	}
    	else
    	{
    		printf("Not Neon Number");
    	}
    }

---
+++ Neon_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,sum=0,sq;
+	scanf("%d",&n);
+	sq=n*n;
+	while(sq!=0)
+	{
+		r=sq%10;
+		sum=sum+r;
+		sq=sq/10;
+	}
+	
+	if(sum==n)
+	{
+		printf("Neon Number");
+	}
+	else
+	{
+		printf("Not Neon Number");
+	}
+}
