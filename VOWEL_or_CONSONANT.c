2dd73d5fd7e27bd754180504d4b055fcd652b7e3
varun1716 <148329234+varun1716@users.noreply.github.com>
2023-11-22 16:09:03 +0530

    #include<stdio.h> int main() {     char x;     scanf("%c",&x);     if(x=='a'||x=='A')     {         printf("VOWEL");     }     else if(x=='e'||x=='E')     {         printf("VOWEL");     }     else if(x=='i'||x=='I')     {         printf("VOWEL");     }     else if(x=='o'||x=='O')     {         printf("VOWEL");     }     else if(x=='u'||x=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    if(x=='a'||x=='A')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='e'||x=='E')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='i'||x=='I')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='o'||x=='O')
+    {
+        printf("VOWEL");
+    }
+    else if(x=='u'||x=='U')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
