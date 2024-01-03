370623e625028600a61067d22d3df7ab58307090
varun1716 <148329234+varun1716@users.noreply.github.com>
2024-01-03 09:58:21 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=1;j<=a;j++)         {             if(i<=j)             printf("* ");             else             printf(" ");         }         printf(" ");     } }

---
+++ Pattern_7.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(i<=j)
+            printf("* ");
+            else
+            printf(" ");
+        }
+        printf("
+");
+    }
+}
