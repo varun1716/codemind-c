3f3f5f8b0c3f2f0b13e5cb8cc4af6bdd7d34af04
varun1716 <148329234+varun1716@users.noreply.github.com>
2024-07-31 22:27:37 +0530

    #include<stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=1;j<=a;j++)         {             if(j==i || j==a-i+1)             printf("%c",i+64);             else             printf(" ");         }         printf(" ");     } }

---
+++ Pattern_8.5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(j==i || j==a-i+1)
+            printf("%c",i+64);
+            else
+            printf(" ");
+        }
+        printf("
+");
+    }
+}
